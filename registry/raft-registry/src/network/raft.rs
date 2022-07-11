3ba545b36a5f4c47280de4724f5dca8a61bb0956
Chen Xu <windoze@0d0a.com>
2022-07-11 21:49:48 +0800

    Missing code check

--- registry/raft-registry/src/network/raft.rs
+++ registry/raft-registry/src/network/raft.rs
@@ -1,17 +1,21 @@
 use openraft::raft::{AppendEntriesRequest, InstallSnapshotRequest, VoteRequest};
+use poem::web::TypedHeader;
 use poem::{
     handler, post,
     web::{Data, Json},
     IntoResponse, Route,
 };
 
-use crate::{RaftRegistryApp, RegistryNodeId, RegistryTypeConfig};
+use crate::{ManagementCode, RaftRegistryApp, RegistryNodeId, RegistryTypeConfig};
 
 #[handler]
 pub async fn vote(
     app: Data<&RaftRegistryApp>,
+    code: Option<TypedHeader<ManagementCode>>,
     req: Json<VoteRequest<RegistryNodeId>>,
 ) -> poem::Result<impl IntoResponse> {
+    app.check_code(code.map(|c| c.0)).await?;
+
     let res = app.raft.vote(req.0).await;
     Ok(Json(res))
 }
@@ -19,8 +23,11 @@ pub async fn vote(
 #[handler]
 pub async fn append(
     app: Data<&RaftRegistryApp>,
+    code: Option<TypedHeader<ManagementCode>>,
     req: Json<AppendEntriesRequest<RegistryTypeConfig>>,
 ) -> poem::Result<impl IntoResponse> {
+    app.check_code(code.map(|c| c.0)).await?;
+
     let res = app.raft.append_entries(req.0).await;
     Ok(Json(res))
 }
@@ -28,8 +35,11 @@ pub async fn append(
 #[handler]
 pub async fn snapshot(
     app: Data<&RaftRegistryApp>,
+    code: Option<TypedHeader<ManagementCode>>,
     req: Json<InstallSnapshotRequest<RegistryTypeConfig>>,
 ) -> poem::Result<impl IntoResponse> {
+    app.check_code(code.map(|c| c.0)).await?;
+
     let res = app.raft.install_snapshot(req.0).await;
     Ok(Json(res))
 }
